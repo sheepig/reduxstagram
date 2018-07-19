816ec30c542cd052839e38d8582b428ae164247f
qianyang.xie <qianyang.xie@vipshop.com>
2018-07-19 20:17:30 +0800

    note1

---
+++ notes/react/react-src/react.js
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import ReactVersion from 'shared/ReactVersion'; // 当前 react 版本
+
+// 查看 react/reactSymbol.js
+// The Symbol used to tag the ReactElement-like types. If there is no native Symbol
+// nor polyfill, then a plain number is used for performance.
+import {
+  REACT_ASYNC_MODE_TYPE,
+  REACT_FRAGMENT_TYPE,
+  REACT_PROFILER_TYPE,
+  REACT_STRICT_MODE_TYPE,
+  REACT_PLACEHOLDER_TYPE,
+} from 'shared/ReactSymbols';
+import {enableSuspense} from 'shared/ReactFeatureFlags';
+
+import {Component, PureComponent} from './ReactBaseClasses';
+import {createRef} from './ReactCreateRef';
+import {forEach, map, count, toArray, only} from './ReactChildren';
+import {
+  createElement,
+  createFactory,
+  cloneElement,
+  isValidElement,
+} from './ReactElement';
+import {createContext} from './ReactContext';
+import forwardRef from './forwardRef';
+import {
+  createElementWithValidation,
+  createFactoryWithValidation,
+  cloneElementWithValidation,
+} from './ReactElementValidator';
+import ReactSharedInternals from './ReactSharedInternals';
+
+const React = {
+  Children: {
+    map,
+    forEach,
+    count,
+    toArray,
+    only,
+  },
+
+  createRef,
+  Component,
+  PureComponent,
+
+  createContext,
+  forwardRef,
+
+  Fragment: REACT_FRAGMENT_TYPE,
+  StrictMode: REACT_STRICT_MODE_TYPE,
+  unstable_AsyncMode: REACT_ASYNC_MODE_TYPE,
+  unstable_Profiler: REACT_PROFILER_TYPE,
+
+  createElement: __DEV__ ? createElementWithValidation : createElement,
+  cloneElement: __DEV__ ? cloneElementWithValidation : cloneElement,
+  createFactory: __DEV__ ? createFactoryWithValidation : createFactory,
+  isValidElement: isValidElement,
+
+  version: ReactVersion,
+
+  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: ReactSharedInternals,
+};
+
+if (enableSuspense) {
+  React.Placeholder = REACT_PLACEHOLDER_TYPE;
+}
+
+export default React;
